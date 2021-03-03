f6d72d0c2b434bfa542eeb8beb9a59dd3ab9af2a
SnehaKarthikeyan <79981351+SnehaKarthikeyan@users.noreply.github.com>
2021-03-03 19:01:16 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,17 @@
+Coding:
+
+n=int(input())
+str1 = input()
+vowels = 0
+consonants = 0
+for i in range(0,n):
+ vowels = 0
+ consonants = 0
+ for i in str1:
+    if(i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u' or i == 'A' or i == 'E' or i == 'I' or i == 'O' or i == 'U'):
+        vowels = vowels + 1
+    else:
+        consonants = consonants + 1
+ 
+ print(vowels,end=" ")
+ print(consonants)
